5aa416beb55bee56d9f639291d7f7a3be1e6b55a
lapmu <antjdgkdl@gmail.com>
2024-01-01 05:50:28 +0900

    ✨feat/myIntro, projects 데이터 구성

---
+++ src/data/projects.ts
@@ -0,0 +1,15 @@
+const data = [
+  {
+    title: '',
+    day: '',
+    people: '',
+    discription: '',
+    github: '',
+    repo: '',
+    stack: ['', ''],
+    part: [{ title: '', discription: ['', ''] }],
+    problems: [{ problem: '', solution: '' }],
+  },
+];
+
+export default data;
