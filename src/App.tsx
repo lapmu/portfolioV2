4743789b84db2efdc33b0c3f12b1b14380320cd3
lapmu <antjdgkdl@gmail.com>
2024-01-05 05:51:09 +0900

    ✨ feat/mainpage 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <img src='main' />
+        </div>
+        <div>
+          <div>👋안녕하세요</div>
+          <div>
+            <div>
+              <img src='my' />
+              <span>email : </span>
+              <span>phone : </span>
+              <div>
+                <img src='github' />
+                <img src='blog' />
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>배운 것을 나누며 성장하는 것을 좋아합니다.</div>
+                <div>스터디를 통해</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
